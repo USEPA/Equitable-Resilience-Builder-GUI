e16645f9d333bae778d21f13bf6cc3b5c4f8bc19
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-17 16:26:34 -0500

    Created Chapter1Panel FXML & Controller

---
+++ erb/app/src/main/java/erb/Chapter1PanelController.java
@@ -0,0 +1,43 @@
+package erb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Chapter1PanelController implements Initializable{
+	
+	@FXML
+	VBox chapter1VBox;
+	@FXML
+	HBox chapter1HBox;
+	@FXML
+	TreeView<String> chapter1TreeView;
+	@FXML
+	VBox chapter1ContentVBox;
+	
+	public Chapter1PanelController() {
+		
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		fillTreeView();
+	}
+	
+	public void fillTreeView() {
+		TreeItem<String> root = new TreeItem<String>("Chapter 1");
+		root.setExpanded(true);
+		root.getChildren().add(new TreeItem<String>("Create Goals"));
+		root.getChildren().add(new TreeItem<String>("Gather Core Team"));
+		root.getChildren().add(new TreeItem<String>("Project Plan"));
+		root.getChildren().add(new TreeItem<String>("Reflection"));
+
+		chapter1TreeView.setRoot(root);
+	}
+
+}
