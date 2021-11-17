0ff94e5dc3f4e96666c1192f6cc0b985361f5fc7
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-17 15:03:36 -0500

    Created ERBTool FXML & Controller

---
+++ erb/app/src/main/java/erb/ERBToolController.java
@@ -0,0 +1,129 @@
+package erb;
+
+import java.net.URL;
+import javafx.fxml.FXML;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ERBToolController implements Initializable {
+
+	@FXML
+	VBox erbVBox;
+	@FXML
+	HBox menuHBox;
+	@FXML
+	Label panel1Label; // Welcome
+	@FXML
+	Label panel2Label; // Chapter 1
+	@FXML
+	Label panel3Label; // Chapter 2
+	@FXML
+	Label panel4Label; // Chapter 3
+	@FXML
+	Label panel5Label; // Chapter 4
+	@FXML
+	Label panel6Label; // Chapter 5
+
+	private Label selectedPanelLabel;
+	private ArrayList<Label> listOfAllPanelLabels;
+
+	public ERBToolController() {
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		listOfAllPanelLabels = createListOfAllPanelLabels();
+
+	}
+
+	@FXML
+	public void panel1LabelAction() {
+		selectedPanelLabel = panel1Label;
+		unHighlightAllMenuLabels();
+		highlightNode(panel1Label);
+
+	}
+
+	@FXML
+	public void panel2LabelAction() {
+		selectedPanelLabel = panel2Label;
+		unHighlightAllMenuLabels();
+		highlightNode(panel2Label);
+
+	}
+
+	@FXML
+	public void panel3LabelAction() {
+		selectedPanelLabel = panel3Label;
+		unHighlightAllMenuLabels();
+		highlightNode(panel3Label);
+
+	}
+
+	@FXML
+	public void panel4LabelAction() {
+		selectedPanelLabel = panel4Label;
+		unHighlightAllMenuLabels();
+		highlightNode(panel4Label);
+
+	}
+
+	@FXML
+	public void panel5LabelAction() {
+		selectedPanelLabel = panel5Label;
+		unHighlightAllMenuLabels();
+		highlightNode(panel5Label);
+
+	}
+
+	@FXML
+	public void panel6LabelAction() {
+		selectedPanelLabel = panel6Label;
+		unHighlightAllMenuLabels();
+		highlightNode(panel6Label);
+
+	}
+
+	public void setAllPanelLabelsHoverAction() {
+		for (Label panelLabel : listOfAllPanelLabels) {
+			if (panelLabel != null) {
+				panelLabel.setOnMouseEntered(e -> highlightNode(panelLabel));
+				panelLabel.setOnMouseExited(e -> unHighlightNode(panelLabel));
+			}
+		}
+	}
+
+	public void unHighlightNode(Node node) {
+		if (node != selectedPanelLabel) {
+			node.setStyle(null);
+		}
+	}
+
+	public void highlightNode(Node node) {
+		node.setStyle("-fx-background-color: #D8D8D8");
+	}
+
+	public void unHighlightAllMenuLabels() {
+		for (Label label : listOfAllPanelLabels) {
+			unHighlightNode(label);
+		}
+	}
+
+	public ArrayList<Label> createListOfAllPanelLabels() {
+		ArrayList<Label> listOfAllPanelLabels = new ArrayList<Label>();
+		listOfAllPanelLabels.add(panel1Label);
+		listOfAllPanelLabels.add(panel2Label);
+		listOfAllPanelLabels.add(panel3Label);
+		listOfAllPanelLabels.add(panel4Label);
+		listOfAllPanelLabels.add(panel5Label);
+		listOfAllPanelLabels.add(panel6Label);
+		return listOfAllPanelLabels;
+	}
+
+}
