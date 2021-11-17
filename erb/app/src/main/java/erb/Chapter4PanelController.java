ea372910b631fb8c425c5b8e340b3c29c5c7fea0
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-17 16:28:32 -0500

    Created Chapter4Panel FXML & Controller

---
+++ erb/app/src/main/java/erb/Chapter4PanelController.java
@@ -0,0 +1,44 @@
+package erb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Chapter4PanelController implements Initializable{
+
+	@FXML
+	VBox chapter4VBox;
+	@FXML
+	HBox chapter4HBox;
+	@FXML
+	TreeView<String> chapter4TreeView;
+	@FXML
+	VBox chapter4ContentVBox;
+	
+	public Chapter4PanelController() {
+		
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		fillTreeView();
+	}
+	
+	public void fillTreeView() {
+		TreeItem<String> root = new TreeItem<String>("Chapter 4");
+		root.setExpanded(true);
+		root.getChildren().add(new TreeItem<String>("Measure Indicators"));
+		root.getChildren().add(new TreeItem<String>("Sort Indicators"));
+		root.getChildren().add(new TreeItem<String>("Score Indicators"));
+		root.getChildren().add(new TreeItem<String>("Visualize Indicators"));
+		root.getChildren().add(new TreeItem<String>("Reflection"));
+
+		chapter4TreeView.setRoot(root);
+	}
+
+}
