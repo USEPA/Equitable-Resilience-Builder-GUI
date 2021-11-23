e124f5dc2205d70bbd084734ff8832cc1ed95d63
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-23 09:24:50 -0500

    Created ProgressTracker FXML & Controller

---
+++ erb/app/src/main/java/erb/ProgressTrackerController.java
@@ -0,0 +1,48 @@
+package erb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+public class ProgressTrackerController implements Initializable{
+
+	@FXML
+	VBox progressTrackerVBox;
+	@FXML
+	ListView<String> goalsListView;
+	@FXML
+	Button addNewGoalButton;
+	@FXML
+	Button addNewTaskButton;
+	@FXML
+	VBox chapter1TasksVBox;
+	@FXML
+	VBox chapter2TasksVBox;
+	@FXML
+	VBox chapter3TasksVBox;
+	@FXML
+	VBox chapter4TasksVBox;
+	@FXML
+	VBox chapter5TasksVBox;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	@FXML
+	public void addNewGoalButtonAction() {
+		
+	}
+	
+	@FXML
+	public void addNewTaskButtonAction() {
+		
+	}
+
+}
