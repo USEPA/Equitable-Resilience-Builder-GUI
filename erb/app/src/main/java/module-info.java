34b4c931418996336f180bcb56604218f80dc330
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-23 14:10:58 -0500

    Added org.json.simple dependency

--- erb/app/src/main/java/module-info.java
+++ erb/app/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module erb {
 	requires transitive javafx.fxml;
 	requires transitive javafx.controls;
 	requires javafx.graphics;
+	requires transitive json.simple;
 	opens erb to javafx.fxml;
 	exports erb;
 }
