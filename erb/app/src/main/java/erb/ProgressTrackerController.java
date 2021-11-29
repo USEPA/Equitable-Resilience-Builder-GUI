0a2734893d4778b30a328d463eb6e23acea77f24
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-29 08:33:49 -0500

    Parse static tasks and goals and add them to their respective trackers

--- erb/app/src/main/java/erb/ProgressTrackerController.java
+++ erb/app/src/main/java/erb/ProgressTrackerController.java
@@ -43,27 +43,36 @@ public class ProgressTrackerController implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		addStaticGoals();
-		addStaticTasks();
+		parseStaticGoals();
+		parseStaticTasks();
 	}
 	
-	public void addStaticGoals() {
+	public void parseStaticGoals() {
 		ParserJSON parserJSON = new ParserJSON();
 		ArrayList<JSONObject> jsonObjects = parserJSON.parseFile("C:\\Users\\AWILKE06\\OneDrive - Environmental Protection Agency (EPA)\\Documents\\Projects\\Metro-CERI\\FY22\\Dev_Docs\\Eclipse_Repo\\Clone_11_17_2021\\MetroCERI\\erb_supporting_docs\\User\\Sessions\\Session_Example\\Goals\\Goals.json", "goals");
-		
 		for(JSONObject jsonObject: jsonObjects) {
-			System.out.println("Goal = " + jsonObject);
+			ERBGoal erbGoal = new ERBGoal();
+			erbGoal.setId(Integer.parseInt(jsonObject.get("id").toString()));
+			erbGoal.setTitle(jsonObject.get("title").toString());
+			erbGoal.setStatic(Boolean.parseBoolean(jsonObject.get("isStatic").toString()));
+			erbGoal.setDescription(jsonObject.get("description").toString());
+			erbGoal.setChapterAssignment(jsonObject.get("chapterAssignment").toString());
+			goalTracker.addGoal(erbGoal);
 		}
 	}
 	
-	public void addStaticTasks() {
+	public void parseStaticTasks() {
 		ParserJSON parserJSON = new ParserJSON();
 		ArrayList<JSONObject> jsonObjects = parserJSON.parseFile("C:\\Users\\AWILKE06\\OneDrive - Environmental Protection Agency (EPA)\\Documents\\Projects\\Metro-CERI\\FY22\\Dev_Docs\\Eclipse_Repo\\Clone_11_17_2021\\MetroCERI\\erb_supporting_docs\\User\\Sessions\\Session_Example\\Tasks\\Tasks.json", "tasks");
-		
 		for(JSONObject jsonObject: jsonObjects) {
-			System.out.println("Task = " + jsonObject);
+			ERBTask erbTask = new ERBTask();
+			erbTask.setId(Integer.parseInt(jsonObject.get("id").toString()));
+			erbTask.setTitle(jsonObject.get("title").toString());
+			erbTask.setStatic(Boolean.parseBoolean(jsonObject.get("isStatic").toString()));
+			erbTask.setDescription(jsonObject.get("description").toString());
+			erbTask.setChapterAssignment(jsonObject.get("chapterAssignment").toString());
+			taskTracker.addTask(erbTask);
 		}
-		
 	}
 	
 	@FXML
