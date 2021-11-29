27933c2ce7f952fa72f0ee10af18a26e6815c0d6
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-11-29 12:44:38 -0500

    Working on wrapping text of tasks

--- erb/app/src/main/java/erb/TaskController.java
+++ erb/app/src/main/java/erb/TaskController.java
@@ -7,9 +7,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.VBox;
 
 public class TaskController implements Initializable{
 
+	@FXML
+	VBox taskVBox;
 	@FXML
 	ProgressIndicator progressIndicator;
 	@FXML
@@ -25,6 +28,9 @@ public class TaskController implements Initializable{
 		progressIndicator.getStylesheets().add("/ProgressIndicator.css");
 		label.setText(erbTask.getTitle());
 		Tooltip.install(label, new Tooltip(erbTask.getDescription()));
+		taskVBox.setId(String.valueOf(erbTask.getId()));
+		label.prefWidthProperty().bind(taskVBox.widthProperty());
+		label.prefHeightProperty().bind(taskVBox.heightProperty());
 
 	}
 }
