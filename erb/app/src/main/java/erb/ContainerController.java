8014cf68e969223368bf87f666d629a29765cf94
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-12-08 13:50:41 -0500

    Methods to clean & load container content

--- erb/app/src/main/java/erb/ContainerController.java
+++ erb/app/src/main/java/erb/ContainerController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.layout.VBox;
 
 public class ContainerController implements Initializable{
@@ -19,6 +20,15 @@ public class ContainerController implements Initializable{
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void cleanVBox(VBox vBox) {
+		vBox.getChildren().clear();
+	}
+	
+	public void loadContent(Node node) {
+		cleanVBox(containerVBox);
+		containerVBox.getChildren().add(node);
 	}
 
 }
