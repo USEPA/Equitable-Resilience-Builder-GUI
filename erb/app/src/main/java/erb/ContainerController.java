057fd024bdd87908096ad75aa67854fe64850e4b
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-12-08 12:59:02 -0500

    Created Container FXML & Controller

---
+++ erb/app/src/main/java/erb/ContainerController.java
@@ -0,0 +1,24 @@
+package erb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class ContainerController implements Initializable{
+
+	public ContainerController() {
+		
+	}
+
+	@FXML
+	VBox containerVBox;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
