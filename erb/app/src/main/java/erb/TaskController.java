33eca7fddeab189e3540f776aa9a9d134d873353
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2021-12-08 13:07:23 -0500

    Added color to tasks text

--- erb/app/src/main/java/erb/TaskController.java
+++ erb/app/src/main/java/erb/TaskController.java
@@ -31,6 +31,22 @@ public class TaskController implements Initializable{
 		taskVBox.setId(String.valueOf(erbTask.getId()));
 		label.prefWidthProperty().bind(taskVBox.widthProperty());
 		label.prefHeightProperty().bind(taskVBox.heightProperty());
-
+		setLabelColor();
+	}
+	
+	public void setLabelColor() {
+		if(erbTask.getChapterAssignment() != null) {
+			if(erbTask.getChapterAssignment().toLowerCase().contains("chapter1")) {
+				label.setStyle("-fx-text-fill: #ef7777");
+			}else if(erbTask.getChapterAssignment().toLowerCase().contains("chapter2")) {
+				label.setStyle("-fx-text-fill: #87b472");
+			}else if(erbTask.getChapterAssignment().toLowerCase().contains("chapter3")) {
+				label.setStyle("-fx-text-fill: #1ccbea");
+			}else if(erbTask.getChapterAssignment().toLowerCase().contains("chapter4")) {
+				label.setStyle("-fx-text-fill: #d699ff");
+			}else if(erbTask.getChapterAssignment().toLowerCase().contains("chapter5")) {
+				label.setStyle("-fx-text-fill: #fc9e2a");
+			}
+		}
 	}
 }
