2b7bbfb2e62f1beb0dcc62dd0262e8446af73dcb
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-11 10:52:51 -0500

    Updated chapter 4 vbox set up

--- erb/app/src/main/java/erb_chapter4/Chap4Step2Controller.java
+++ erb/app/src/main/java/erb_chapter4/Chap4Step2Controller.java
@@ -11,6 +11,8 @@ import javafx.scene.layout.VBox;
 public class Chap4Step2Controller implements Initializable{
 
 	@FXML
+	VBox panelVBox;
+	@FXML
 	VBox contentVBox;
 	
 	WizardContainerController wizardContainerController;
@@ -22,6 +24,6 @@ public class Chap4Step2Controller implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		chapter_Handler.addNavigationPanel(contentVBox, wizardContainerController);
+		chapter_Handler.addNavigationPanel(panelVBox, wizardContainerController);
 	}
 }
