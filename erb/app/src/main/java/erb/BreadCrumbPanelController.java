980daecc81395f2f162cb2a2455454809d7543f9
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-12 14:14:43 -0500

    Code to determine the selected bread crumb bar id

--- erb/app/src/main/java/erb/BreadCrumbPanelController.java
+++ erb/app/src/main/java/erb/BreadCrumbPanelController.java
@@ -2,6 +2,9 @@ package erb;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -18,8 +21,12 @@ public class BreadCrumbPanelController implements Initializable{
 	BreadCrumbBar<String> breadCrumbBar;
 	
 	int chapterNum;
-	public BreadCrumbPanelController(int chapterNum) {
+	WizardContainerController wizardContainerController;
+	Wizard wizard;
+	public BreadCrumbPanelController(int chapterNum, WizardContainerController wizardContainerController, Wizard wizard) {
 		this.chapterNum = chapterNum;
+		this.wizardContainerController = wizardContainerController;
+		this.wizard = wizard;
 	}
 
 	@Override
@@ -28,9 +35,15 @@ public class BreadCrumbPanelController implements Initializable{
 		setBreadCrumbBar();
 	}
 	
+	public void setBreadCrumbBarCrumb(int index) {
+		
+	}
+	
 	public void setBreadCrumbBar() {
 		Label selectedCrumbLabel = new Label();
 		breadCrumbBar = new BreadCrumbBar<>();
+		breadCrumbBar.setAutoNavigationEnabled(false);
+		breadCrumbBar.getStylesheets().add("/BreadCrumbBar.css");
 		breadCrumbVBox.getChildren().addAll(breadCrumbBar, selectedCrumbLabel);
 		if(chapterNum == 1) {
 			breadCrumbBar.setSelectedCrumb(getChapter1BreadCrumbs());
@@ -47,34 +60,45 @@ public class BreadCrumbPanelController implements Initializable{
 	}
 	
 	public TreeItem<String> getChapter1BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Chapter 1", "Step 1", "Step 2", "Step 3", "Step 4", "Reflect");
+		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
 		return model;
 	}
 	
 	public TreeItem<String> getChapter2BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Chapter 2", "Step 1", "Step 2", "Step 3", "Step 4", "Reflect");
+		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
 		return model;
 	}
 	
 	public TreeItem<String> getChapter3BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Chapter 3", "Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Reflect");
+		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6");
 		return model;
 	}
 	
 	public TreeItem<String> getChapter4BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Chapter 4", "Step 1", "Step 2", "Step 3", "Reflect");
+		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4");
 		return model;
 	}
 	
 	public TreeItem<String> getChapter5BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Chapter 5", "Step 1", "Step 2", "Step 3", "Step 4", "Reflect");
+		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
 		return model;
 	}
 	
 	public void handleBreadCrumbEvent(BreadCrumbActionEvent<String> event) {
-		System.out.println("You just clicked on '" + event.getSelectedCrumb() + "'!");
-		clearOldBreadCrumbBar();
-		setBreadCrumbBar();
+		String selectedCrumbPanelId = getSelectedBreadCrumbBarId(event);
+		int panelId = wizard.getPanelIndex(selectedCrumbPanelId);
+		wizardContainerController.loadWizardPanel(panelId);
+	}
+	
+	public String getSelectedBreadCrumbBarId(BreadCrumbActionEvent<String> event) {
+		String selectedCrumbText = event.getSelectedCrumb().getValue();
+		Pattern pattern = Pattern.compile("[0-9]");
+		Matcher matcher = pattern.matcher(selectedCrumbText);
+		if(matcher.find()) {
+			String selectedCrumbStepNumber = matcher.group(0);
+			return chapterNum + "." + selectedCrumbStepNumber;
+		}
+		return "";
 	}
 	
 	public void clearOldBreadCrumbBar() {
