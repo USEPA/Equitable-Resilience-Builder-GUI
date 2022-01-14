a7b2d5654a87d5af5b3f26ec47f135808c0a2b75
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-14 10:51:34 -0500

    Lists to hold the panels from each chapter separately

--- erb/app/src/main/java/erb/Wizard.java
+++ erb/app/src/main/java/erb/Wizard.java
@@ -39,13 +39,33 @@ public class Wizard {
 	
 	WizardContainerController wizardContainerController;
 	ArrayList<Node> listOfPanelsInWizard = new ArrayList<Node>();
+	ArrayList<Node> listOfChapter1PanelsInWizard = new ArrayList<Node>();
+	ArrayList<Node> listOfChapter2PanelsInWizard = new ArrayList<Node>();
+	ArrayList<Node> listOfChapter3PanelsInWizard = new ArrayList<Node>();
+	ArrayList<Node> listOfChapter4PanelsInWizard = new ArrayList<Node>();
+	ArrayList<Node> listOfChapter5PanelsInWizard = new ArrayList<Node>();
 	HashMap<Integer, Node> mapOfPanelsInWizard = new HashMap<Integer, Node>();
 	
-	public void addPanel(Node node) {
+	public void addPanel(Node node, int chapterNumber) {
 		if(node != null) {
 			listOfPanelsInWizard.add(node);
 			int numPanels = mapOfPanelsInWizard.keySet().size();
 			mapOfPanelsInWizard.put(numPanels, node);
+			addPanelToChapterList(node, chapterNumber);
+		}
+	}
+	
+	public void addPanelToChapterList(Node node, int chapterNumber) {
+		if(chapterNumber == 1) {
+			listOfChapter1PanelsInWizard.add(node);
+		}else if (chapterNumber ==2) {
+			listOfChapter2PanelsInWizard.add(node);
+		}else if (chapterNumber ==3) {
+			listOfChapter3PanelsInWizard.add(node);
+		}else if (chapterNumber ==4) {
+			listOfChapter4PanelsInWizard.add(node);
+		}else if (chapterNumber ==5) {
+			listOfChapter5PanelsInWizard.add(node);
 		}
 	}
 	
@@ -210,6 +230,26 @@ public class Wizard {
 	public ArrayList<Node> getListOfPanelsInWizard() {
 		return listOfPanelsInWizard;
 	}
+	
+	public ArrayList<Node> getListOfChapter1PanelsInWizard() {
+		return listOfChapter1PanelsInWizard;
+	}
+
+	public ArrayList<Node> getListOfChapter2PanelsInWizard() {
+		return listOfChapter2PanelsInWizard;
+	}
+
+	public ArrayList<Node> getListOfChapter3PanelsInWizard() {
+		return listOfChapter3PanelsInWizard;
+	}
+
+	public ArrayList<Node> getListOfChapter4PanelsInWizard() {
+		return listOfChapter4PanelsInWizard;
+	}
+
+	public ArrayList<Node> getListOfChapter5PanelsInWizard() {
+		return listOfChapter5PanelsInWizard;
+	}
 
 	public HashMap<Integer, Node> getMapOfPanelsInWizard() {
 		return mapOfPanelsInWizard;
@@ -228,7 +268,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap1Step1.fxml"));
 			Chap1Step1Controller chap1Step1Controller = new Chap1Step1Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap1Step1Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 1);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -239,7 +279,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap1Step2.fxml"));
 			Chap1Step2Controller chap1Step2Controller = new Chap1Step2Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap1Step2Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 1);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -250,7 +290,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap1Step3.fxml"));
 			Chap1Step3Controller chap1Step3Controller = new Chap1Step3Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap1Step3Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 1);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -261,7 +301,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap1Step4.fxml"));
 			Chap1Step4Controller chap1Step4Controller = new Chap1Step4Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap1Step4Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 1);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -272,7 +312,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap1Step5.fxml"));
 			Chap1Step5Controller chap1Step5Controller = new Chap1Step5Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap1Step5Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 1);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -283,7 +323,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap2Step1.fxml"));
 			Chap2Step1Controller chap2Step1Controller = new Chap2Step1Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap2Step1Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 2);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -294,7 +334,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap2Step2.fxml"));
 			Chap2Step2Controller chap2Step2Controller = new Chap2Step2Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap2Step2Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 2);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -305,7 +345,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap2Step3.fxml"));
 			Chap2Step3Controller chap2Step3Controller = new Chap2Step3Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap2Step3Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 2);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -316,7 +356,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap2Step4.fxml"));
 			Chap2Step4Controller chap2Step4Controller = new Chap2Step4Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap2Step4Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 2);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -327,7 +367,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap2Step5.fxml"));
 			Chap2Step5Controller chap2Step5Controller = new Chap2Step5Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap2Step5Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 2);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -338,7 +378,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap3Step1.fxml"));
 			Chap3Step1Controller chap3Step1Controller = new Chap3Step1Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap3Step1Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 3);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -349,7 +389,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap3Step2.fxml"));
 			Chap3Step2Controller chap3Step2Controller = new Chap3Step2Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap3Step2Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 3);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -360,7 +400,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap3Step3.fxml"));
 			Chap3Step3Controller chap3Step3Controller = new Chap3Step3Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap3Step3Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 3);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -371,7 +411,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap3Step4.fxml"));
 			Chap3Step4Controller chap3Step4Controller = new Chap3Step4Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap3Step4Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 3);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -382,7 +422,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap3Step5.fxml"));
 			Chap3Step5Controller chap3Step5Controller = new Chap3Step5Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap3Step5Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 3);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -393,7 +433,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap3Step6.fxml"));
 			Chap3Step6Controller chap3Step6Controller = new Chap3Step6Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap3Step6Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 3);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -404,7 +444,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap4Step1.fxml"));
 			Chap4Step1Controller chap4Step1Controller = new Chap4Step1Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap4Step1Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 4);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -415,7 +455,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap4Step2.fxml"));
 			Chap4Step2Controller chap4Step2Controller = new Chap4Step2Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap4Step2Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 4);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -426,7 +466,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap4Step3.fxml"));
 			Chap4Step3Controller chap4Step3Controller = new Chap4Step3Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap4Step3Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 4);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -437,7 +477,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap4Step4.fxml"));
 			Chap4Step4Controller chap4Step4Controller = new Chap4Step4Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap4Step4Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 4);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -448,7 +488,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap5Step1.fxml"));
 			Chap5Step1Controller chap5Step1Controller = new Chap5Step1Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap5Step1Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 5);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -459,7 +499,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap5Step2.fxml"));
 			Chap5Step2Controller chap5Step2Controller = new Chap5Step2Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap5Step2Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 5);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -470,7 +510,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap5Step3.fxml"));
 			Chap5Step3Controller chap5Step3Controller = new Chap5Step3Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap5Step3Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 5);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -481,7 +521,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap5Step4.fxml"));
 			Chap5Step4Controller chap5Step4Controller = new Chap5Step4Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap5Step4Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 5);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -492,7 +532,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Chap5Step5.fxml"));
 			Chap5Step5Controller chap5Step5Controller = new Chap5Step5Controller(wizardContainerController, this);
 			fxmlLoader.setController(chap5Step5Controller);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), 5);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -503,7 +543,7 @@ public class Wizard {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Final.fxml"));
 			FinalController finalController = new FinalController();
 			fxmlLoader.setController(finalController);
-			addPanel(fxmlLoader.load());
+			addPanel(fxmlLoader.load(), -1);
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
