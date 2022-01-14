e39c2b9dca8541415e4e69d81484d6261a389cbd
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-14 10:38:55 -0500

    Updated bread crumb bar to use manual navigation instead of javafx BreadCrumbBar

--- erb/app/src/main/java/erb/BreadCrumbPanelController.java
+++ erb/app/src/main/java/erb/BreadCrumbPanelController.java
@@ -2,15 +2,11 @@ package erb;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TreeItem;
-import javafx.scene.layout.VBox;
-import org.controlsfx.control.BreadCrumbBar;
-import org.controlsfx.control.BreadCrumbBar.BreadCrumbActionEvent;
+import javafx.scene.layout.HBox;
 
 public class BreadCrumbPanelController implements Initializable{
 
@@ -23,84 +19,133 @@ public class BreadCrumbPanelController implements Initializable{
 		this.chapterNum = chapterNum;
 		this.wizardContainerController = wizardContainerController;
 	}
-	
-	BreadCrumbBar<String> breadCrumbBar;
-	
+		
 	@FXML
-	VBox breadCrumbVBox;
+	HBox breadCrumbHBox;
+	@FXML
+	Button step1Button;
+	@FXML
+	Button step2Button;
+	@FXML
+	Button step3Button;
+	@FXML
+	Button step4Button;
+	@FXML
+	Button step5Button;
+	@FXML
+	Button step6Button;
+	@FXML
+	Label arrowLabel1;
+	@FXML
+	Label arrowLabel2;
+	@FXML
+	Label arrowLabel3;
+	@FXML
+	Label arrowLabel4;
+	@FXML
+	Label arrowLabel5;
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		setBreadCrumbBar();
-	}
-	
-	public void setBreadCrumbBarCrumb(int index) {
-		
-	}
-	
-	public void setBreadCrumbBar() {
-		Label selectedCrumbLabel = new Label();
-		breadCrumbBar = new BreadCrumbBar<>();
-		breadCrumbBar.setAutoNavigationEnabled(false);
-		breadCrumbBar.getStylesheets().add("/BreadCrumbBar.css");
-		breadCrumbVBox.getChildren().addAll(breadCrumbBar, selectedCrumbLabel);
 		if(chapterNum == 1) {
-			breadCrumbBar.setSelectedCrumb(getChapter1BreadCrumbs());
-		}else if (chapterNum ==2) {
-			breadCrumbBar.setSelectedCrumb(getChapter2BreadCrumbs());
-		}else if (chapterNum ==3) {
-			breadCrumbBar.setSelectedCrumb(getChapter3BreadCrumbs());
+			setChapter1Controls();
+		}else if(chapterNum == 2) {
+			setChapter2Controls();
+		}else if (chapterNum == 3) {
+			setChapter3Controls();
 		}else if (chapterNum == 4) {
-			breadCrumbBar.setSelectedCrumb(getChapter4BreadCrumbs());
+			setChapter4Controls();
 		}else if (chapterNum == 5) {
-			breadCrumbBar.setSelectedCrumb(getChapter5BreadCrumbs());
+			setChapter5Controls();
 		}
-		breadCrumbBar.setOnCrumbAction(e -> handleBreadCrumbEvent(e));
 	}
 	
-	public TreeItem<String> getChapter1BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
-		return model;
+	@FXML
+	public void step1ButtonAction() {
+		String panelAccessibleTextId = chapterNum + ".1";
+		wizardContainerController.loadWizardPanel(panelAccessibleTextId);
 	}
 	
-	public TreeItem<String> getChapter2BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
-		return model;
+	@FXML
+	public void step2ButtonAction() {
+		String panelAccessibleTextId = chapterNum + ".2";
+		wizardContainerController.loadWizardPanel(panelAccessibleTextId);
 	}
 	
-	public TreeItem<String> getChapter3BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6");
-		return model;
+	@FXML
+	public void step3ButtonAction() {
+		String panelAccessibleTextId = chapterNum + ".3";
+		wizardContainerController.loadWizardPanel(panelAccessibleTextId);
 	}
 	
-	public TreeItem<String> getChapter4BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4");
-		return model;
+	@FXML
+	public void step4ButtonAction() {
+		String panelAccessibleTextId = chapterNum + ".4";
+		wizardContainerController.loadWizardPanel(panelAccessibleTextId);
 	}
 	
-	public TreeItem<String> getChapter5BreadCrumbs() {
-		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
-		return model;
+	@FXML
+	public void step5ButtonAction() {
+		String panelAccessibleTextId = chapterNum + ".5";
+		wizardContainerController.loadWizardPanel(panelAccessibleTextId);
 	}
 	
-	public void handleBreadCrumbEvent(BreadCrumbActionEvent<String> event) {
-		String selectedCrumbPanelId = getSelectedBreadCrumbBarId(event);
-		int panelId = wizard.getIndex(selectedCrumbPanelId);
-		wizardContainerController.loadWizardPanel(panelId);
+	@FXML
+	public void step6ButtonAction() {
+		String panelAccessibleTextId = chapterNum + ".6";
+		wizardContainerController.loadWizardPanel(panelAccessibleTextId);
 	}
 	
-	public String getSelectedBreadCrumbBarId(BreadCrumbActionEvent<String> event) {
-		String selectedCrumbText = event.getSelectedCrumb().getValue();
-		Pattern pattern = Pattern.compile("[0-9]");
-		Matcher matcher = pattern.matcher(selectedCrumbText);
-		if(matcher.find()) {
-			String selectedCrumbStepNumber = matcher.group(0);
-			return chapterNum + "." + selectedCrumbStepNumber;
-		}
-		return null;
+	public void setChapter1Controls() {
+		breadCrumbHBox.getChildren().remove(step6Button);
+		breadCrumbHBox.getChildren().remove(arrowLabel5);
 	}
 	
-	public void clearOldBreadCrumbBar() {
-		breadCrumbVBox.getChildren().clear();
+	public void setChapter2Controls() {
+		breadCrumbHBox.getChildren().remove(step6Button);
+		breadCrumbHBox.getChildren().remove(arrowLabel5);
 	}
+	
+	public void setChapter3Controls() {
+		//Leave as is
+	}
+	
+	public void setChapter4Controls() {
+		breadCrumbHBox.getChildren().remove(step5Button);
+		breadCrumbHBox.getChildren().remove(arrowLabel4);
+		breadCrumbHBox.getChildren().remove(step6Button);
+		breadCrumbHBox.getChildren().remove(arrowLabel5);
+	}
+	
+	public void setChapter5Controls() {
+		breadCrumbHBox.getChildren().remove(step6Button);
+		breadCrumbHBox.getChildren().remove(arrowLabel5);
+	}
+	
+//	public TreeItem<String> getChapter1BreadCrumbs() {
+//		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
+//		return model;
+//	}
+//	
+//	public TreeItem<String> getChapter2BreadCrumbs() {
+//		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
+//		return model;
+//	}
+//	
+//	public TreeItem<String> getChapter3BreadCrumbs() {
+//		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6");
+//		return model;
+//	}
+//	
+//	public TreeItem<String> getChapter4BreadCrumbs() {
+//		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4");
+//		return model;
+//	}
+//	
+//	public TreeItem<String> getChapter5BreadCrumbs() {
+//		TreeItem<String> model = BreadCrumbBar.buildTreeModel("Step 1", "Step 2", "Step 3", "Step 4", "Step 5");
+//		return model;
+//	}
+	
 }
