9707d95d6f7b852ae32ec5a340887b91138755a2
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-25 09:55:53 -0500

    Added Chap1Landing FXML & Controller

---
+++ erb/app/src/main/java/erb_chapter1/Chap1LandingController.java
@@ -0,0 +1,35 @@
+package erb_chapter1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import erb.Chapter_Handler;
+import erb.Wizard;
+import erb.WizardContainerController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class Chap1LandingController implements Initializable{
+
+	Wizard wizard;
+	WizardContainerController wizardContainerController;
+	
+	public Chap1LandingController(WizardContainerController wizardContainerController, Wizard wizard) {
+		this.wizard = wizard;
+		this.wizardContainerController = wizardContainerController;
+	}
+	
+	Chapter_Handler chapter_Handler = new Chapter_Handler();
+	
+	@FXML
+	VBox panelVBox;
+	@FXML
+	VBox contentVBox;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		chapter_Handler.addNavigationPanel(panelVBox, wizardContainerController, wizard);
+	}
+
+}
