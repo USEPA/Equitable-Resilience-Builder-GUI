7c833f87b9323e57d727edeadd298c88d5926cdc
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-26 16:42:17 -0500

    Added step landing fxml & controller to all chapter 4 steps

---
+++ erb/app/src/main/java/erb_chapter4/Chap4Step4LandingController.java
@@ -0,0 +1,34 @@
+package erb_chapter4;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import erb.Chapter_Handler;
+import erb.Wizard;
+import erb.WizardContainerController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class Chap4Step4LandingController implements Initializable{
+
+	Wizard wizard;
+	WizardContainerController wizardContainerController;
+	
+	public Chap4Step4LandingController (WizardContainerController wizardContainerController, Wizard wizard) {
+		this.wizard = wizard;
+		this.wizardContainerController = wizardContainerController;
+	}
+	
+	Chapter_Handler chapter_Handler = new Chapter_Handler();
+	
+	@FXML
+	VBox panelVBox;
+	@FXML
+	VBox contentVBox;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		chapter_Handler.addNavigationPanel(panelVBox, wizardContainerController, wizard);		
+	}
+
+}
