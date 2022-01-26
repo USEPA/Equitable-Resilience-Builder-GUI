7aa741b5bc9a0d7b9508db39bc708ad70cec8cd8
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-01-26 14:38:01 -0500

    Added step landing fxml & controller to all chapter 1 steps

---
+++ erb/app/src/main/java/erb_chapter1/Chap1Step5LandingController.java
@@ -0,0 +1,33 @@
+package erb_chapter1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import erb.Chapter_Handler;
+import erb.Wizard;
+import erb.WizardContainerController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class Chap1Step5LandingController implements Initializable{
+	Wizard wizard;
+	WizardContainerController wizardContainerController;
+	
+	public Chap1Step5LandingController (WizardContainerController wizardContainerController, Wizard wizard) {
+		this.wizard = wizard;
+		this.wizardContainerController = wizardContainerController;
+	}
+	
+	Chapter_Handler chapter_Handler = new Chapter_Handler();
+	
+	@FXML
+	VBox panelVBox;
+	@FXML
+	VBox contentVBox;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		chapter_Handler.addNavigationPanel(panelVBox, wizardContainerController, wizard);		
+	}
+
+}
