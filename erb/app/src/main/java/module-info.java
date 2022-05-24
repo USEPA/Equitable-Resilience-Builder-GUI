cfebcb99ff4bb7f2edc35f3aff5d47c47df5f40d
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-05-24 10:14:10 -0400

    Removed unused classes

--- erb/app/src/main/java/module-info.java
+++ erb/app/src/main/java/module-info.java
@@ -18,6 +18,5 @@ module com.epa.erb {
 	opens com.epa.erb.noteboard to javafx.fxml;
 	opens com.epa.erb.worksheet to javafx.fxml, javafx.web;
 	opens com.epa.erb.engagement_action to javafx.fxml;
-	opens com.epa.erb.engagement_setup to javafx.fxml;
 	exports com.epa.erb;
 }
