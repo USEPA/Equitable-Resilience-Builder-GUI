48ed2c6a9ab3c3ed634a2b499e2047753b6217cd
Audrey Wilkinson <wilkinson.audrey@epa.gov>
2022-12-06 13:19:39 -0500

    Remove start a new project from ProjectSelection

--- erb/app/src/main/java/com/epa/erb/project/ProjectSelectionController.java
+++ erb/app/src/main/java/com/epa/erb/project/ProjectSelectionController.java
@@ -1,27 +1,21 @@
 package com.epa.erb.project;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import com.epa.erb.App;
 import com.epa.erb.goal.Goal;
 import com.epa.erb.goal.GoalCategory;
 import com.epa.erb.goal.GoalCreationController;
-import com.epa.erb.utility.FileHandler;
 import com.epa.erb.utility.MainPanelHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.text.Font;
 import javafx.util.Callback;
 
@@ -39,8 +33,8 @@ public class ProjectSelectionController implements Initializable{
 		this.mode = mode;
 	}
 	
-	private FileHandler fileHandler = new FileHandler();
-	private Logger logger = LogManager.getLogger(ProjectSelectionController.class);
+//	private FileHandler fileHandler = new FileHandler();
+//	private Logger logger = LogManager.getLogger(ProjectSelectionController.class);
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -55,18 +49,18 @@ public class ProjectSelectionController implements Initializable{
 //		app.getErbContainerController().setTitleLabelText("ERB: Project Selection");
 	}
 	
-	@FXML
-	public void createButtonAction() {
-		String newProjectName = projectNameTextField.getText().trim();
-		if(isValidNewProjectName(newProjectName)) {
-			String cleanedProjectName = cleanStringForWindows(newProjectName);
-			Project project = new Project(newProjectName, mode, cleanedProjectName);
-			createNewProjectDirectory(project);
-			addProjectToListView(project);
-			projectsListView.getSelectionModel().select(project);
-			mouseClickedProject(null);
-		}
-	}
+//	@FXML
+//	public void createButtonAction() {
+//		String newProjectName = projectNameTextField.getText().trim();
+//		if(isValidNewProjectName(newProjectName)) {
+//			String cleanedProjectName = cleanStringForWindows(newProjectName);
+//			Project project = new Project(newProjectName, mode, cleanedProjectName);
+//			createNewProjectDirectory(project);
+//			addProjectToListView(project);
+//			projectsListView.getSelectionModel().select(project);
+//			mouseClickedProject(null);
+//		}
+//	}
 	
 	private GoalCreationController createFacilitatorProject(Project project) {
 		GoalCreationController goalCreationController = new GoalCreationController(app, project, this);
@@ -104,34 +98,34 @@ public class ProjectSelectionController implements Initializable{
 		app.loadNodeToERBContainer(engagementActionRoot);
 	}
 	
-	private boolean isValidNewProjectName(String projectName) {
-		if (projectName != null && projectName.length() > 0) {
-			if (!isDuplicateProjectName(projectName)) {
-				return true;
-			} else {
-				showIsDuplicateProjectNameAlert();
-			}
-		}
-		return false;
-	}
+//	private boolean isValidNewProjectName(String projectName) {
+//		if (projectName != null && projectName.length() > 0) {
+//			if (!isDuplicateProjectName(projectName)) {
+//				return true;
+//			} else {
+//				showIsDuplicateProjectNameAlert();
+//			}
+//		}
+//		return false;
+//	}
+//	
+//	private String cleanStringForWindows(String string) {
+//		return string.replaceAll("[^A-Za-z0-9]", "_");
+//	}
 	
-	private String cleanStringForWindows(String string) {
-		return string.replaceAll("[^A-Za-z0-9]", "_");
-	}
-	
-	private boolean isDuplicateProjectName(String projectName) {
-		if (projectName != null) {
-			ArrayList<Project> existingProjects = app.getProjects();
-			for (Project project : existingProjects) {
-				if (projectName.contentEquals(project.getProjectName())) {
-					return true;
-				}
-			}
-			return false;
-		} else {
-			return true;
-		}
-	}
+//	private boolean isDuplicateProjectName(String projectName) {
+//		if (projectName != null) {
+//			ArrayList<Project> existingProjects = app.getProjects();
+//			for (Project project : existingProjects) {
+//				if (projectName.contentEquals(project.getProjectName())) {
+//					return true;
+//				}
+//			}
+//			return false;
+//		} else {
+//			return true;
+//		}
+//	}
 	
 	private boolean isProjectNew(Project project) {
 		if (project != null) {
@@ -145,34 +139,34 @@ public class ProjectSelectionController implements Initializable{
 		}
 	}
 	
-	private void showIsDuplicateProjectNameAlert() {
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setHeaderText(null);
-		alert.setContentText("Duplicate project name. Please enter a new project name.");
-		alert.setTitle("Error");
-		alert.showAndWait();
-	}
-
-	private void createNewProjectDirectory(Project project) {
-		if (project != null) {
-			File newProjectDirectory = fileHandler.getProjectDirectory(project);
-			if (newProjectDirectory!=null && !newProjectDirectory.exists()) {
-				newProjectDirectory.mkdir();
-			}
-		} else {
-			logger.error("Cannot createNewProjectDirectory. project is null.");
-		}
-	}
-	
-	private void addProjectToListView(Project project) {
-		if (project != null) {
-			if (!projectsListView.getItems().contains(project)) {
-				projectsListView.getItems().add(project);
-			}
-		} else {
-			logger.error("Cannot addProjectToListView. project is null.");
-		}
-	}
+//	private void showIsDuplicateProjectNameAlert() {
+//		Alert alert = new Alert(AlertType.ERROR);
+//		alert.setHeaderText(null);
+//		alert.setContentText("Duplicate project name. Please enter a new project name.");
+//		alert.setTitle("Error");
+//		alert.showAndWait();
+//	}
+//
+//	private void createNewProjectDirectory(Project project) {
+//		if (project != null) {
+//			File newProjectDirectory = fileHandler.getProjectDirectory(project);
+//			if (newProjectDirectory!=null && !newProjectDirectory.exists()) {
+//				newProjectDirectory.mkdir();
+//			}
+//		} else {
+//			logger.error("Cannot createNewProjectDirectory. project is null.");
+//		}
+//	}
+//	
+//	private void addProjectToListView(Project project) {
+//		if (project != null) {
+//			if (!projectsListView.getItems().contains(project)) {
+//				projectsListView.getItems().add(project);
+//			}
+//		} else {
+//			logger.error("Cannot addProjectToListView. project is null.");
+//		}
+//	}
 	
 	private void fillProjectsListView() {
 		ArrayList<Project> projects = app.getProjects();
